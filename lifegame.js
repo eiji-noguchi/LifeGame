b2d60131b8d4e0e353f22eb8fc92419097402a75
eiji-noguchi <eiji.program@gmail.com>
2019-04-02 03:21:06 +0900

    LifeGame

--- lifegame.js
+++ lifegame.js
@@ -26,7 +26,7 @@ window.onload = function () {
  * @param {function} callback jsonファイル読み込み成功時実行関数
  */
 function readFile(filename, callback) {
-    var rep = new XMLHttpRequest();
+    var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (req.readyState === 4) {
             if (req.status === 200) {
@@ -74,9 +74,9 @@ state.create = function (nx, ny) {
     state.nx = nx;
     state.ny = ny;
     // セルを表す2次元配列を生成し初期化
-    state.cells = [ny];
+    state.cells = new Array(ny);
     for (var ix = 0; ix < nx; ix++) {
-        state.cells[ix] = [ny];
+        state.cells[ix] = new Array(ny);
         for (var iy = 0; iy < ny; iy++) {
             // cells[ix][iy]が0のときは生物がいない、1のときは生物がいる
             state.cells[ix][iy] = 0;
@@ -167,16 +167,266 @@ state.update = function () {
  * セルの状態を設定
  * セル(ix,iy)に対してlifeが0のとき生物を死滅、1のとき生物を誕生、2のとき生物の生死を逆転させる
  */
-state.setLife=function(ix,iy,life){
-    if(life === 2){
+state.setLife = function (ix, iy, life) {
+    if (life === 2) {
         state.cells[ix][iy] ^= 1;
-        state.tellCellChange(ix,iy,state.cells[ix][iy]);
-    }else{
-        if(state.cells[ix][iy] != life){
+        state.tellCellChange(ix, iy, state.cells[ix][iy]);
+    } else {
+        if (state.cells[ix][iy] != life) {
             state.cells[ix][iy] = life;
-            state.tellCellChange(ix,iy,life);
+            state.tellCellChange(ix, iy, life);
         }
     }
     // 世代を0としコールバックする
-    state.tellGenerationChange(state.generation=0);
+    state.tellGenerationChange(state.generation = 0);
+}
+
+/**
+ * viewオブジェクトを定義
+ */
+view.create = function (nx, ny, width, height) {
+    // レイヤーを表すcanvas要素を生成
+    view.layer = [];
+    // 生物表示用
+    view.layer[0] = elt("canvas", { id: "rayer0", width: width, height: height });  // 生物を描画
+    view.layer[1] = elt("canvas", { id: "rayer1", width: width, height: height });  // 格子線を描画
+    // 格子サイズ、セルのサイズ、生物マーカーの半径を設定
+    view.nx = nx;
+    view.ny = ny;
+    // セルの幅
+    view.cellWidth = view.layer[0].width / nx;
+    // セルの高さ
+    view.cellHeight = view.layer[0].height.ny;
+    // 生物を表す円の半径
+    view.markRadius = (Math.min(view.cellWidth, view.cellHeight / 2.5 + 0.5)) | 0
+    // canvasコンテキストを取得
+    if (view.ctx) {
+        delete view.ctx
+    }
+    view.ctx = [];
+    for (var i = 0; i < view.layer.length; i++) {
+        view.ctx.push(view.layer[i].getContext("2d"));
+    }
+    // 描画パラメータの初期設定
+    view.backColor = "forestgreen"; // 背景色
+    view.markColor = "white";       // 生物の色
+    view.strokeStyle = "black";     // 格子線の色
+    view.lineWidth = 0.2;           // 格子線の幅
+    // 格子を描画する
+    view.drawLattice();
+    // 世代数を表示する要素の生成
+    view.generation = elt("span", { id: "generation" });
+    view.statuspanel = elt("div", { class: "status" }, "世代数:", view.generation);
+
+    //clickviewイベント用のイベントオブジェクトを生成
+    view.clickEvent = document.createEvent("HTMLEvents");
+    // layer[1](格子)をクリックしたときのイベントリスナを登録
+    view.layer[1].addEventListener("click", function (e) {
+        // セルのx方向の番号
+        var ix = Math.floor(e.offsetX / view.cellWidth);
+        // セルのy方向の番号
+        var iy = Math.floor(e.offsetY / view.cellWidth);
+        // viewの(ix,iy)がクリックされたことをclickviewイベントで通知
+        view.clickEvent.initEvent("clickview", false, false);
+        view.clickEvent.detail = { ix: ix, iy: iy, life: 2 };
+        document.dispatchEvent(view.clickEvent);
+    }, false);
+    // changeCellイベントリスナの登録:stateからのイベントでセルを再描画する
+    document.addEventListener("changegeneration", function (e) {
+        view.showGeneration(e.detail.generation);
+    }, false);
+
+    //viewpanel要素オブジェクトを返す
+    return elt("div", { class: "viewpanel" }, view.layer[0], view.layer[1], view.statuspanel);
+};
+
+/**
+ * 格子線を描画する
+ */
+view.drawLattice = function () {
+    // 各レイヤーのCanvasをリセット
+    for (var i = 0; i < view.layer.length; i++) {
+        view.layer[i].width = view.layer[i].width
+    }
+    // layer[1]に格子を描く。講師はnxが150未満のときに描く
+    if (view.nx < 150) {
+        var c = view.ctx[1];
+        c.lineWidth = view.lineWidth;
+        c.strokeStyle = view.strokeStyle;
+        for (var ix = 0; ix <= view.nx; ix++) {
+            c.beginPath();
+            c.moveTo(ix * view.cellWidth, 0);
+            c.lineTo(ix * view.cellWidth, view.nx * view.cellHeight);
+            c.stroke();
+        }
+        for (var iy = 0; iy <= view.ny; iy++) {
+            c.beginPath();
+            c.moveTo(0, iy * view.cellHeight);
+            c.lineTo(view.nx * view.cellWidth, iy * view.cellHeight);
+            c.stroke();
+        }
+    }
+    // layer[0]を背景色で塗りつぶす
+    c = view.ctx[0];
+    c.fillStyle = view.backColor;
+    c.fillRect(0, 0, view.layer[0].width, view.layer[0].height);
+};
+
+/**
+ * セルに生物を描画する
+ */
+view.drawCell = function (ix, iy, life) {
+    // 生物はlayer[0]に描画する
+    var c = view.ctx[0];
+    c.beginPath();
+    if (life) {
+        // 生物マークを描く
+        var x = (ix + 0.5) * view.cellWidth;
+        var y = (iy + 0.5) * view.cellHeight;
+        var r = view.markRadius;
+        c.fillStyle = view.markColor;
+        c.arc(x, y, r, 0, Math.PI * 2, true);
+        c.fill();
+    } else {
+        // セルを背景色で塗りつぶす
+        var x = ix * view.cellWidth;
+        var y = iy * view.cellHeight;
+        c.fillStyle = view.backColor;
+        c.fillRect(x, y, view.cellWidth, view.cellHeight);
+    }
+};
+
+/**
+ * 世代数の表示
+ */
+view.showGeneration = function (generation) {
+    view.generation.innerHTML = generation;
+}
+
+/**
+ * Contorolesオブジェクトを定義
+ */
+controls.play = function (state) {
+    if (!state.timeInterval) {
+        state.timeInterval = 300;
+    }
+    var input = elt("input", { type: "button", value: "連続再生" });
+    input.addEventListener("click", function (e) {
+        if (!state.playing) {
+            state.timer = setInterval(state.update, state.timeInterval);
+            state.playing = true;
+        }
+    });
+    return input;
+};
+
+/**
+ * 再生速度選択
+ */
+controls.changeTimeInterval = function (state) {
+    var select = elt("select");
+    var options = [
+        { name: "超高速(20ms)", value: 20 },
+        { name: "高速(100ms)", value: 100 },
+        { name: "標準(300ms)", value: 300 },
+        { name: "低速(600ms)", value: 600 },
+    ];
+    for (var i = 0; i < options.length; i++) {
+        var option = elt("option", null, options[i].name);
+        select.appendChild(option);
+    }
+    select.selectedIndex = 2;
+    select.addEventListener("change", function (e) {
+        SVGMetadataElement.timeInterval = options[select.selectedIndex].value;
+        if (state.playing) {
+            clearInterval(state.timer);
+            state.timer = setInterval(state.update, state.timeInterval);
+        }
+    });
+    return select
+};
+
+/**
+ * 停止ボタン
+ */
+controls.stop = function (state) {
+    var input = elt("input", { type: "button", value: "停止" });
+    input.addEventListener("click", function (e) {
+        if (state.playing) {
+            clearInterval(state.timer);
+            state.playing = false;
+        }
+    });
+    return input;
+};
+
+/**
+ * 次へボタン
+ */
+controls.step = function (state) {
+    var input = elt("input", { type: "button", value: "次へ" });
+    input.addEventListener("click", function (e) {
+        clearInterval(state.timer);
+        state.playing = false;
+        state.update();
+    });
+    return input;
+};
+
+/**
+ * パターン選択
+ */
+controls.pattern = function (state) {
+    var select = elt("select");
+    select.appendChild(elt("option", null, "パターン選択"));
+    for (var i = 0; i < state.patterns.length; i++) {
+        select.appendChild(elt("option", null, state.patterns[i].name));
+    }
+    select.selectedIndex = 0;
+    select.addEventListener("change", function (e) {
+        clearInterval(state.timer);
+        state.playing = false;
+        if (select.selectedIndex != 0) {
+            placePatttern(state.patterns[select.selectedIndex - 1]);
+        }
+        select.selectedIndex = 0;
+    });
+    return select;
+    function placePatttern(pattern) {
+        var array = pattern.points;
+        // x,yの最小値と最大値を求める
+        var max = [0, 0];
+        var min = [state.nx - 1, state.ny - 1];
+        for (var i = 0; i < array.length; i++) {
+            for (var d = 0; d < 2; d++) {
+                if (array[i][d] > max[d]) {
+                    max[d] = array[i][d];
+                }
+                if(array[i][d]<min[d]){
+                    min[d]=array[i][d];
+                }
+            }
+        }
+        // 全セルを削除
+        state.clearAllCell();
+        // Canvasの中心にパターンを配置
+        for(var i=0; i<array.length; i++){
+            var ix = array[i][0]+Math.floor((state.nx-min[0]-max[0])/2);
+            var iy = array[i][1]+Math.floor((state.ny-min[1]-max[1])/2);
+            state.setLife(ix,iy,1);
+        }
+        state.tellGenerationChange(state.generation=0);
+    }
+};
+
+/**
+ * 消去ボタン
+ */
+controls.clear = function(state){
+    var input = elt("input",{type:"button",value:"全消去"});
+    input.addEventListener("clicl",function(e){
+        clearInterval(state.timer);
+        state.playing=false;
+    });
+    return input;
 }
